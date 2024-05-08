a31cb050e8abc5230248a633ec5d9816a405acd2
Nay Lin Aung <naylinaung.it.dev@gmail.com>
2024-05-08 20:22:44 +0630

    stimulus js testing

--- app/javascript/controllers/welcome_controller.js
+++ app/javascript/controllers/welcome_controller.js
@@ -2,14 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="welcome"
 export default class extends Controller {
-  static targets = ["name","greeting"]
+  static targets = ["name","greeting", "yourName", "getDatetime"]
   connect() {
   }
 
   greet(){
     this.greetingTarget.textContent = `hello, ${this.name}`;
+    this.getDatetimeTarget.textContent = Date();
   }
   get name(){
     return this.nameTarget.value;
+  }
+
+  alert_testing(){
+    alert(`Hello ${this.yourname}, Thanks for Button clicked!`);
+    this.yourNameTarget.value = "";
+  }
+
+  get yourname(){
+    return this.yourNameTarget.value;
   }
 }
